fb8e82920cafee0e1433075abd72b0f843829c1f
Jose Narvaez <goyox86@gmail.com>
2017-03-21 00:19:09 +0000

    Parsing locations fromn CLI args.

--- src/location.rs
+++ src/location.rs
@@ -1,3 +1,9 @@
+use std::str::FromStr;
+use std::convert::From;
+use std::num::ParseFloatError;
+use std::error;
+use std::fmt;
+
 use units::Kilometers;
 
 const EARTH_RADIUS_IN_KM: f64 = 6371f64;
@@ -29,5 +35,44 @@ impl Location {
         let c = 2.0f64 * a.sqrt().atan2((1.0f64 - a).sqrt());
 
         Kilometers(EARTH_RADIUS_IN_KM * c)
+    }
+}
+
+#[derive(Debug)]
+pub struct ParseLocationError(String);
+
+impl From<ParseFloatError> for ParseLocationError {
+    fn from(parse_err: ParseFloatError) -> Self {
+        ParseLocationError(format!("error parsing location {}", parse_err))
+    }
+}
+
+impl FromStr for Location {
+    type Err = ParseLocationError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let coordinates: Vec<&str> = s.split(",").collect();
+        if coordinates.len() < 2 {
+            return Err(ParseLocationError(String::from("missing element latitude,longitude on tuple")))
+        }
+        let latitude = f64::from_str(coordinates[0])?;
+        let longitude = f64::from_str(coordinates[1])?;
+
+        Ok(Location::new(latitude, longitude))
+    }
+}
+
+impl fmt::Display for ParseLocationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Location parse error: {}", self.0)
+    }
+}
+
+impl error::Error for ParseLocationError {
+    fn description(&self) -> &str {
+        &self.0
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        Some(self)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -19,32 +19,36 @@ use units::Kilometers;
 use customer_locator::CustomerLocator;
 use customer_datasource::CustomerJSONFile;
 
-const DEFAULT_CUSTOMERS_FILE: &str = "data/customers.json";
-const DEFAULT_RADIUS: &str = "100";
+const DEFAULT_ARG_CUSTOMERS_FILE: &str = "data/customers.json";
+const DEFAULT_ARG_RADIUS_IN_KM: &str = "100";
+const DEFAULT_ARG_LOCATION: &str = "53.3393,-6.2576841"; // Dublin
 
 fn main() {
     let matches = App::new("CustomerLocator")
-        .version("1.0")
+        .version("0.1.0")
         .author("Jose Narvaez. <goyox86@gmail.com>")
-        .about("Locates customers near Dublin.")
+        .about("Locates customers near a given location.")
         .arg(Arg::with_name("file")
             .short("f")
             .long("file")
             .value_name("FILE")
             .help("The input file with the customers")
-            .default_value(DEFAULT_CUSTOMERS_FILE)
+            .default_value(DEFAULT_ARG_CUSTOMERS_FILE)
             .takes_value(true))
         .arg(Arg::with_name("radius")
             .short("r")
             .long("radius")
             .value_name("RADIUS")
             .help("The radius of the search in Kilometers")
-            .default_value(DEFAULT_RADIUS)
+            .default_value(DEFAULT_ARG_RADIUS_IN_KM)
             .takes_value(true))
-        .arg(Arg::with_name("debug")
-            .short("d")
-            .multiple(true)
-            .help("Turn debugging information on"))
+        .arg(Arg::with_name("location")
+            .short("l")
+            .long("location")
+            .value_name("LOCATION")
+            .help("The location for what customers are gonna be located. In the format latitude,longitude.")
+            .default_value(DEFAULT_ARG_LOCATION)
+            .takes_value(true))
         .get_matches();
 
     // Parsing input file name
@@ -55,20 +59,29 @@ fn main() {
     let radius = match f64::from_str(radius_str) {
         Ok(radius) => radius,
         Err(err) => {
-            println!("Invalid radius: {}", err);
+            println!("{}", err);
+            return;
+        }
+    };
+
+    // Parsing the location
+    let location_str = matches.value_of("location").unwrap();
+    let location = match Location::from_str(location_str) {
+        Ok(location) => location,
+        Err(err) => {
+            println!("{}", err);
             return;
         }
     };
 
     let customers_json_file = CustomerJSONFile::new(Path::new(input_file_path));
     let locator = CustomerLocator::from_source(customers_json_file);
-    let dublin = Location::new(53.3393f64, -6.2576841f64);
 
-    let mut customers = locator.locate_within(&Kilometers(radius), &dublin);
+    let mut customers = locator.locate_within(&Kilometers(radius), &location);
     customers.as_mut_slice().sort_by(|first, second| first.user_id.cmp(&second.user_id));
 
     for customer in customers {
-        let dist_from_dublin = customer.location().distance_from(&dublin);
+        let dist_from_dublin = customer.location().distance_from(&location);
         println!("Distance from Dublin for {}: {} is {}",
                  customer.name,
                  customer.user_id,
